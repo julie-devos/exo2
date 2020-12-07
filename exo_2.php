057444293c069fa078cee7ea3f21cd15d4fda085
julie-devos <julie.devos@epitech.eu>
2020-12-07 15:01:47 +0100

    commit

---
+++ exo_2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>PhpPart1Exo2</title>
+</head>
+<body>
+	<?php
+
+	$lastName="julie";
+	$firstName="Devos";
+	$age=27;
+	?>
+	<p><?= ''.$firsName.' '.lastName.' est âgé de '.$age.''?><p>
+</body>
+</html>
